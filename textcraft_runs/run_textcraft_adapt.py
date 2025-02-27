fc3d0c778f08bfc50a86c470f85106bd8da4683d
Nikolai Rozanov <nikolai.rozanov@gmail.com>
2025-02-27 11:02:00 +0000

    added act only

--- textcraft_runs/run_textcraft_adapt.py
+++ textcraft_runs/run_textcraft_adapt.py
@@ -970,6 +970,84 @@ acition: None
 '''
 
 
+# NO GOAL
+atomic_examples_act_only = {
+'craft_with_ingredients':'''Crafting commands:
+craft 3 dark oak sign using 6 dark oak planks, 1 stick
+craft 4 dark oak planks using 1 dark oak log
+craft 1 stick using 1 planks
+craft 4 stick using 2 bamboo
+craft 4 oak planks using 1 oak log
+craft 1 dark oak fence using 2 stick, 4 dark oak planks
+craft 1 warped stairs using 6 warped planks
+craft 3 oak sign using 6 oak planks, 1 stick
+
+Goal: craft dark oak sign
+
+> action: inventory
+
+Inventory: [stick] (1) [dark oak planks] (6)
+
+> action: get dark oak sign
+
+Could not find dark oak sign
+
+> action: inventory
+
+Inventory: [stick] (1) [dark oak planks] (6)
+
+> action: craft 3 dark oak sign using 6 dark oak planks, 1 stick
+
+Crafted 3 minecraft:dark_oak_sign
+
+> action: inventory
+
+Inventory: [dark oak sign] (3)
+''', 
+'craft_with_ingredients_gen':'''Goal: fetch 2 dark oak logs.
+
+> action: inventory
+
+Inventory: [stick] (1)
+
+> action: get 2 dark oak logs
+
+Got 2 dark oak logs
+
+> action: inventory
+
+Inventory: [dark oak log] (2) [stick] (1)
+'''
+}
+
+act_only_trajectory = '''Goal: craft 2 oak planks
+
+> action: intentory
+
+Inventory: [stick] (1) 
+
+> action: get 2 oak planks
+
+Could not find 2 oak planks
+
+> action: inventory
+
+Inventory: [stick] (1) 
+
+> action: get 1 oak log
+
+Got 1 oak log
+
+> action: craft 4 oak planks using 1 oak log
+
+Crafted 4 minecraft:oak_planks
+
+> action: inventory
+
+Inventory: [stick] (1) [oak_planks] (4)
+'''
+
+
 
 
 
@@ -1106,6 +1184,15 @@ elif AGENT_TYPE=="stateact_no_goal":
     atomic_exec_prompt +=  '\n\n'.join(atomic_examples_stateact_no_goal[k] for k in atomic_examples_stateact_no_goal.keys()) + '\n'
     # Pure Stateact
     atomic_exec_prompt += 'Here is an example of a complex goal.\n\n' + stateact_no_goal_trajectory + '\n'
+
+elif AGENT_TYPE=="act":
+    # STATEACT ALTERNATIVE
+    atomic_exec_prompt+="""\n\nHere is a demo of how to fetch and craft objects.\n\n"""
+    atomic_exec_prompt +=  '\n\n'.join(atomic_examples_act_only[k] for k in atomic_examples_act_only.keys()) + '\n'
+    # Pure Stateact
+    atomic_exec_prompt += 'Here is an example of a complex goal.\n\n' + act_only_trajectory + '\n'
+
+
 
 atomic_exec_prompt += "Now here is a different goal. You can use these crafting commands to accomplish the goal. When you the desired item in your inventory, think: Task Completed! If you have tried your best but cannot proceed, think: task failed!\n" 
 
